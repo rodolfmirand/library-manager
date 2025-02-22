fa4decbe6fcd80171a1bb836890e6822e96d4f40
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-22 16:14:13 -0300

    add glogal prefix and enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('library')
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   const port = 8080
   await app.listen(process.env.PORT ?? 8080);
+
   console.log(`Nest server running on port: ${port}`)
 }
 bootstrap();
