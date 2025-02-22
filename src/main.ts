fef1478e9320d8025c0f2cdc68d9fa1215babebf
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-22 14:54:36 -0300

    add console.log()

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = 8080
+  await app.listen(process.env.PORT ?? 8080);
+  console.log(`Nest server running on port: ${port}`)
 }
 bootstrap();
