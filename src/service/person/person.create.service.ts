aef4d1cac8a42d5ad0a3b198e4b1bb0a02898fef
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-22 16:14:30 -0300

    create person create service

---
+++ src/service/person/person.create.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Person } from "src/model/person.model";
+import { Repository } from "typeorm";
+import { PersonRequest } from '../../dto/person.request';
+
+@Injectable()
+export class PersonCreateService {
+
+    constructor(@InjectRepository(Person) private model: Repository<Person>) { }
+
+    public async create(personRequest: PersonRequest): Promise<any> {
+        await this.model.save(personRequest)
+        return { status: 'Person created successfully!' }
+    }
+}
