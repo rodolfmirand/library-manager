d1b2c63c88d7a5da86274289d0c98e8deeeb1467
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-23 16:11:47 -0300

    add person class in imports

--- src/module/person.module.ts
+++ src/module/person.module.ts
@@ -2,12 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { PersonCreateController } from "src/controller/person/person.create.controller";
 import { PersonFindAllController } from "src/controller/person/person.findall.controller";
+import { Book } from "src/model/book.model";
 import { Person } from "src/model/person.model";
 import { PersonCreateService } from "src/service/person/person.create.service";
 import { PersonFindAllService } from "src/service/person/person.findall.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Person])],
+    imports: [TypeOrmModule.forFeature([Person, Book])],
     controllers: [PersonCreateController, PersonFindAllController],
     providers: [PersonCreateService, PersonFindAllService]
 })
