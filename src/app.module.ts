dc3eee4efe69e4736326eca06e529afc25275fb6
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-23 16:14:08 -0300

    add exchange module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from './module/person.module';
 import { BookModule } from './module/book.module';
+import { ExchangeModule } from './module/exchange.module';
 
 @Module({
-    imports: [PersonModule, BookModule, TypeOrmModule.forRoot({
+    imports: [PersonModule, BookModule, ExchangeModule, TypeOrmModule.forRoot({
         "database": "./database.sql",
         "type": "sqlite",
         "synchronize": true,
