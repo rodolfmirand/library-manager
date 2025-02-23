2587616af2ef99208df9ea7db9a7da2c2d86b118
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-22 22:16:14 -0300

    create book findall service

---
+++ src/service/book/book.findall.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Book } from "src/model/book.model";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class BookFindAllService {
+
+    constructor(@InjectRepository(Book) private model: Repository<Book>) { }
+
+    public async find(): Promise<Book[]> {
+        return await this.model.find()
+    }
+}
