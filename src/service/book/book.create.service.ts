3cb2802862db91ae95c364a8293f0dd045c00fc3
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-23 16:12:43 -0300

    modify book create service: person register books

--- src/service/book/book.create.service.ts
+++ src/service/book/book.create.service.ts
@@ -1,16 +1,26 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { BookRequest } from "src/dto/book.request";
 import { Book } from "src/model/book.model";
+import { Person } from "src/model/person.model";
 import { Repository } from "typeorm";
 
 @Injectable()
 export class BookCreateService {
 
-    constructor(@InjectRepository(Book) private model: Repository<Book>) { }
+    constructor(@InjectRepository(Book) private bookRepository: Repository<Book>,
+        @InjectRepository(Person) private personRepository: Repository<Person>) { }
 
-    public async create(bookRequest: BookRequest): Promise<any> {
-        await this.model.save(bookRequest)
+    public async create(idPerson: string, bookRequest: BookRequest): Promise<any> {
+        const person = await this.personRepository.findOne({ where: { id: idPerson }, relations: ['booksForExchange'] })
+
+        if (!person)
+            throw new NotFoundException('Person not found')
+
+        const book = this.bookRepository.create({ ...bookRequest, owner: person })
+
+        await this.bookRepository.save(book)
+
         return { status: 'Book registered successfully!' }
     }
 }
