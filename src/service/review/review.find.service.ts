6909bf737d26268c2d35f937ef9ed76c55e1e25a
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-02-26 08:57:45 -0300

    create review find service

---
+++ src/service/review/review.find.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Review } from "src/model/review.model";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class ReviewFindService {
+
+    constructor(@InjectRepository(Review) private reviewRepository: Repository<Review>) { }
+
+    public async find(idBook: string): Promise<any> {
+        const reviews = await this.reviewRepository.find({ where: { book: { id: idBook } }, relations: ['book'] })
+
+        if (!reviews) {
+            throw new NotFoundException('Book not found.')
+        }
+
+        return reviews
+    }
+}
