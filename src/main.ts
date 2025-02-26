539594f9a20c77f22c76942a90588a4b9bd7ca46
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-02-26 07:55:43 -0300

    enable cors 5173 port

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('library')
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
-  const port = 8080
   await app.listen(process.env.PORT ?? 8080);
 
-  console.log(`Nest server running on port: ${port}`)
 }
 bootstrap();
