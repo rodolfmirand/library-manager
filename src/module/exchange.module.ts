d94717076bf1eb673c3e6b88ac42284907c3dd98
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2025-02-26 10:57:57 -0300

    add find all controller and service

--- src/module/exchange.module.ts
+++ src/module/exchange.module.ts
@@ -1,14 +1,16 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ExchangeCreateController } from "src/controller/exchange/exchange.create.controller";
+import { ExchangeFindAllController } from "src/controller/exchange/exchange.findall.controller";
 import { Book } from "src/model/book.model";
 import { Exchange } from "src/model/exchange.model";
 import { Person } from "src/model/person.model";
 import { ExchangeCreateService } from "src/service/exchange/exchange.create.service";
+import { ExchangeFindAllService } from "src/service/exchange/exchange.findall.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Exchange, Person, Book])],
-    controllers: [ExchangeCreateController],
-    providers: [ExchangeCreateService]
+    controllers: [ExchangeCreateController, ExchangeFindAllController],
+    providers: [ExchangeCreateService, ExchangeFindAllService]
 })
 export class ExchangeModule { }
