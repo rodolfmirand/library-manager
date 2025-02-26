8dc45da97edb0b823ac65d1b882fffd0cc035c65
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2025-02-26 18:23:02 -0300

    add turn available controller and service

--- src/module/book.module.ts
+++ src/module/book.module.ts
@@ -3,15 +3,17 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { BookCreateController } from "src/controller/book/book.create.controller";
 import { BookFindAllByPersonController } from "src/controller/book/book.findall.byperson.controller";
 import { BookFindAllController } from "src/controller/book/book.findall.controller";
+import { BookTurnAvailableController } from "src/controller/book/book.turnavailable.controller";
 import { Book } from "src/model/book.model";
 import { Person } from "src/model/person.model";
 import { BookCreateService } from "src/service/book/book.create.service";
 import { BookFindAllByPersonService } from "src/service/book/book.findall.byperson.service";
 import { BookFindAllService } from "src/service/book/book.findall.service";
+import { BookTurnAvailableService } from "src/service/book/book.turnavailable.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Book, Person])],
-    controllers: [BookCreateController, BookFindAllController, BookFindAllByPersonController],
-    providers: [BookCreateService, BookFindAllService, BookFindAllByPersonService]
+    controllers: [BookCreateController, BookFindAllController, BookFindAllByPersonController, BookTurnAvailableController],
+    providers: [BookCreateService, BookFindAllService, BookFindAllByPersonService, BookTurnAvailableService]
 })
 export class BookModule { }
